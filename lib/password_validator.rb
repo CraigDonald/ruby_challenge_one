5118793caf27b86dc48bacd2b61dde26c987df11
Craig Donald <1craig.donald@gmail.com>
2022-10-27 17:26:59 +0100

    Validator 1st ed

---
+++ lib/password_validator.rb
@@ -0,0 +1,11 @@
+# Valid passowords are
+# - 8 or more characters
+# - include at least one of these characters: !, @, $, %, &
+
+def valid?(password)
+    if password.length > 7 && password.include?("!") || password.length > 7 && password.include?("@") || password.length > 7 && password.include?("$") || password.length > 7 && password.include?("%") || password.length > 7 && password.include?("&")
+        then true
+    else
+        false
+    end
+end

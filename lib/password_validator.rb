9625bedd2e034f00baa72a31c9d2457e5897e733
Craig Donald <1craig.donald@gmail.com>
2022-10-28 10:21:33 +0100

    ready to be reviewed

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -3,9 +3,15 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
-    if password.length > 7 && password.include?("!") || password.length > 7 && password.include?("@") || password.length > 7 && password.include?("$") || password.length > 7 && password.include?("%") || password.length > 7 && password.include?("&")
+    if password.length > 7 && password.include?("!")
+    elsif password.length > 7 && password.include?("@") 
+    elsif password.length > 7 && password.include?("$") 
+    elsif password.length > 7 && password.include?("%") 
+    elsif password.length > 7 && password.include?("&")
         then true
     else
         false
     end
 end
+
+

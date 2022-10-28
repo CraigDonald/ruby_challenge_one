bf898494d465424df1768904a717f0bc81c1c286
Craig Donald <1craig.donald@gmail.com>
2022-10-28 14:11:08 +0100

    First refactor

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -2,6 +2,12 @@
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
 
+=begin
+    
+rescue => exception
+    
+    Initial Attempt
+end
 def valid?(password)
      if password.length > 7 && password.include?("!") || password.length > 7 && password.include?("@") || password.length > 7 && password.include?("$") || password.length > 7 && password.include?("%") || password.length > 7 && password.include?("&")
     
@@ -10,5 +16,47 @@ def valid?(password)
         false
     end
 end
+=end
 
+# First Refactor
+
+
+
+def sufficient_length?(password)
+    # this password.length > 7 will evaluate to true or false
+    return password.length > 7
+end
+
+
+def special_chars_included?(password)
+    if password.include?("!")
+      return true
+    elsif password.include?("@")
+      return true
+    elsif password.include?("$")
+      return true
+    elsif password.include?("%")
+      return true
+    elsif password.include?("&")
+      return true
+    else
+      return false
+    end
+  end
+
+  def valid?(password)
+    if sufficient_length?(password) && special_chars_included?(password)
+        return true
+    else
+        return false
+    end
+end
+
+def valid_weak?(password)
+    if special_chars_included?(password)
+      return true
+    else
+      return false
+    end
+  end
 
